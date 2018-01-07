75170bbb3fc20fe465acd9b6aecaa77c560150e7
hashkrish <santhanam.hdw@gmail.com>
2018-01-08 03:26:10 +0530

    added youngs_modulus_python_script.ipynb, scripted for finding molecular strain

---
+++ youngs_modulus_python_script/.ipynb_checkpoints/input-checkpoint.py
@@ -0,0 +1,5 @@
+#set inputs
+
+class Input:
+    
+    def __init__(self,x_,y_ ,)
---
+++ youngs_modulus_python_script/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,9 @@
+foo = 14124
+class Test:
+    a=1325
+    def __init__(self,x1=12,y1=23):
+        x=x1
+        y=y1
+    def fn(x):
+        return x*9
+    
